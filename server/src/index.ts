5a370e314f8819d85218124298568049adeb6c64
Aflu <mohamedaflah186@gmail.com>
2024-09-05 11:41:55 +0530

    feat: server setup completed with node.js api

---
+++ server/src/index.ts
@@ -0,0 +1,36 @@
+import express, { Application } from "express";
+import { createServer } from "http";
+import "dotenv/config";
+import "./infra/socket/index";
+import cors from "cors";
+import "./config/mongo.config";
+import cookieParser from "cookie-parser";
+import userRouter from "./routes/user.router";
+import propertyRoute from "./routes/property.route";
+import serverInstance from "./infra/socket/index";
+import chatRouter from "./routes/chatandMessage";
+const app: Application = express();
+
+app.use(
+  cors({
+    origin: "*",
+    credentials: true,
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
+app.use(cookieParser());
+app.use(express.json());
+
+app.use("/api/user", userRouter);
+app.use("/api/property", propertyRoute);
+app.use("/api/chat", chatRouter);
+
+const server = createServer(app);
+
+serverInstance(server);
+
+server.listen(5000, () => {
+  console.log("Server running on port 4000");
+});
+
+export default app;
