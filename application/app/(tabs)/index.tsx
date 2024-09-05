85ba0c6e0f23ba2168d101b0c9e5137b08de3d39
Aflu <mohamedaflah186@gmail.com>
2024-09-05 21:16:45 +0530

    feat: hero section done

--- application/app/(tabs)/index.tsx
+++ application/app/(tabs)/index.tsx
@@ -4,6 +4,7 @@ import {
   SafeAreaView,
   ScrollView,
   Text,
+  TouchableOpacity,
   View,
 } from "react-native";
 import { GlobalStye } from "../../styles/globa.style";
@@ -24,14 +25,106 @@ const HomePage = () => {
             style={{ width: "100%", height: "100%" }}
           >
             <View
-              style={{ width: "100%", marginTop: 18, paddingHorizontal: 23,display:"flex" }}
+              style={{
+                width: "100%",
+                marginTop: 18,
+                paddingHorizontal: 23,
+                display: "flex",
+              }}
             >
-              <Text style={{ color: "white", fontSize: 43,fontWeight:"bold" }}>
+              <Text
+                style={{ color: "white", fontSize: 43, fontWeight: "bold" }}
+              >
                 Buy, rent, or sell your property easily
               </Text>
-              <Text style={{ color: "white", fontSize: 20,fontWeight:"bold",marginTop:10 }}>
-              A great platform to buy, sell, or even rent your properties without any commisions.
+              <Text
+                style={{
+                  color: "white",
+                  fontSize: 20,
+                  fontWeight: "bold",
+                  marginTop: 10,
+                }}
+              >
+                A great platform to buy, sell, or even rent your properties
+                without any commisions.
               </Text>
+              <View
+                style={{
+                  marginTop: 25,
+                  width: "90%",
+                  marginHorizontal: "auto",
+                  height: 100,
+                  borderRadius: 10,
+                  backgroundColor: "rgba(255 255 255 / 0.8)",
+                }}
+              >
+                <View
+                  style={{
+                    width: "100%",
+                    flexDirection: "row",
+                    height: "35%",
+                    borderBottomWidth: 1,
+                    borderBottomColor: "#6058B9",
+                  }}
+                >
+                  <View
+                    style={{
+                      width: "33.333%",
+                      height: "100%",
+                      alignItems: "center",
+                      justifyContent: "center",
+                      borderBottomColor: "#1400FF",
+                      borderBottomWidth: 2,
+                    }}
+                  >
+                    <Text style={{ fontSize: 18, color: "#6058B9" }}>Rent</Text>
+                  </View>
+                  <View
+                    style={{
+                      width: "33.333%",
+                      height: "100%",
+                      alignItems: "center",
+                      justifyContent: "center",
+                    }}
+                  >
+                    <Text style={{ fontSize: 18, color: "#6058B9" }}>Buy</Text>
+                  </View>
+                  <View
+                    style={{
+                      width: "33.333%",
+                      height: "100%",
+                      alignItems: "center",
+                      justifyContent: "center",
+                    }}
+                  >
+                    <Text style={{ fontSize: 18, color: "#6058B9" }}>
+                      Lease
+                    </Text>
+                  </View>
+                </View>
+                <View
+                  style={{
+                    width: "100%",
+                    height: "65%",
+                    alignItems: "center",
+                    justifyContent: "center",
+                  }}
+                >
+                  <TouchableOpacity
+                    style={{
+                      backgroundColor: "#7065F0",
+                      height: 40,
+                      width: "80%",
+                      marginHorizontal: "auto",
+                      borderRadius: 5,
+                      alignItems: "center",
+                      justifyContent: "center",
+                    }}
+                  >
+                    <Text style={{ color: "white",fontSize:17,fontWeight:500 }}>Brows properties</Text>
+                  </TouchableOpacity>
+                </View>
+              </View>
             </View>
           </ImageBackground>
         </View>
